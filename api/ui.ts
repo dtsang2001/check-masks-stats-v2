6ef0eaf48c6cb1dfe49d4f7606558d1318e96a56
Đinh Thế Sang <thesang201@gmail.com>
2024-08-19 13:54:05 +0700

    first commit

---
+++ api/ui.ts
@@ -0,0 +1,29 @@
+import { colors, createSystem } from 'frog/ui'
+ 
+export const {
+  Box,
+  Columns,
+  Column,
+  Heading,
+  HStack,
+  Rows,
+  Row,
+  Spacer,
+  Text,
+  Divider,
+  VStack,
+  Image,
+  vars,
+} = createSystem({
+    colors: {
+      text: '#000000',
+      main: '#6d30e2',
+      background: '#ffffff',
+      green: '#00ff00',
+      white: '#ffffff',
+      red: '#ff0000',
+      orange: '#ffaa00',
+      gray: "rgb(149 155 157)",
+      blue: 'rgb(0 199 255)'
+    }
+})
